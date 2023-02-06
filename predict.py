a91a26d3bccbf18f9b45d9be5c063fb66968483b
EF <efraisma.ef7@gmail.com>
2023-02-06 16:01:36 -0600

    Update predict.py
    Prediction and graph comparison finished! :smile:

--- predict.py
+++ predict.py
@@ -1,21 +1,20 @@
+from numpy import array
 from pathlib import Path
-from numpy import array, int16
-from wave import open as open_wave
+from itertools import chain
 from keras.models import load_model
-from librosa import load as load_audio
+from librosa.display import waveshow, specshow
 from scipy.io.wavfile import write as write_audio
-from matplotlib.pylab import frombuffer, specgram
-from matplotlib.pyplot import subplot, plot, title, show
+from matplotlib.pyplot import subplot, title, suptitle, show
+from librosa import load as load_audio, stft, amplitude_to_db
 
 
 class CleanHeartSounds:
     def __init__(self) -> None:
         pass
 
-
-    def get_audiofiles(self, audiofiles_dir: str, model_input_shape: int) -> None:
+    def get_audios(self, audiofiles_dir: str, model_input_shape: int) -> None:
         """
-        Import audiofiles, split them in batches and build the entire 
+        Import audiofiles, split them in batches and build the entire
         array of audio-arrays, keeping a list of its original audiofile
         name and the nth-batch
 
@@ -33,7 +32,6 @@ class CleanHeartSounds:
         self.names = []
 
         for audiofile in self.audiofiles_dir.glob("*.wav"):
-            # sampling_rate = 22050
             audio, sampling_rate = load_audio(audiofile)
 
             for i in range(0, len(audio), model_input_shape):
@@ -44,29 +42,118 @@ class CleanHeartSounds:
                     chunk = chunk.reshape((model_input_shape, -1))
                     self.audios.append(chunk)
 
-                    n_chunk = str(i // model_input_shape + 1)
-                    new_name = f'{audiofile.stem}_{n_chunk.zfill(3)}.wav'
-                    self.names.append(new_name)
+                    self.names.append(audiofile.stem)
 
         self.audios = array(self.audios)
+        self.names = dict(enumerate(self.names))
 
     def predict(self, model_dir: str) -> array:
+        """
+        Import the model and predict
+
+        Args: model_dir: string directory name where 
+            the model is placed
+
+        Returns: an array representing the prediction
+        """
+        
         self.model = load_model(model_dir)
         clean = self.model.predict(self.audios)
         return clean
 
+    def group_clean(self) -> None:
+        """
+        Reverses the self.names dict attribute to have
+        which indexes of the prediction batches are linked
+        to the same original image
+        """
+        
+        self.grouped = {}
+        for x, y in self.names.items():
+            if y not in self.grouped:
+                self.grouped[y] = [x]
+            else:
+                self.grouped[y].append(x)
+
     def save_clean(self) -> array:
-        self.clean_dir = self.audiofiles_dir.joinpath('clean')
+        """
+        Creates a prediction folder, join th prediction
+        batches and export them
+        """
+        
+        self.clean_dir = self.audiofiles_dir.joinpath("clean")
         self.clean_dir.mkdir(exist_ok=True)
 
-        for chunk, name in zip(self.clean, self.names):
-            write_audio(name, 22050, chunk)
+        for name, indexes in self.grouped.items():
+            to_save = self.clean[indexes]
+            to_save = list(chain(*to_save))
+            to_save = array(to_save)
 
-    def clean_heart_sounds(self, audiofiles_dir: str, model_dir: str, model_input_shape= int) -> array:
-        self.get_audiofiles(audiofiles_dir, model_input_shape)
+            new_name = f"{name}_clean.wav"
+            new_dir = self.clean_dir.joinpath(new_name)
+            write_audio(new_dir, 22050, to_save)
+
+    def clean_heart_sounds(
+        self, audiofiles_dir: str, model_dir: str, model_input_shape=int
+    ) -> array:
+        """
+        The one and only method to import data, predict and export results
+
+        Args:
+            audiofiles_dir: arg for self.get_audios(...) method
+            model_dir: arg for self.predict(...) method
+            model_input_shape: arg for self.get_audios(...) method
+
+        Returns: an array representing the prediction
+        """
+        
+        self.get_audios(audiofiles_dir, model_input_shape)
         self.clean = self.predict(model_dir)
+        self.group_clean()
         self.save_clean()
         return self.clean
+
+    def compare_clean(self, audiofile_dir: str) -> None:
+        """
+        Import audio file and show its original and cleaned
+        waveform and spectogram representation.
+
+        Args:
+            audiofile_dir: directory string name of
+                the single original heartsound audio file
+
+        Returns: None, waveform and spectogram comparison is displayed
+        """
+
+        orig_dir = Path(audiofile_dir)
+        audio_name = orig_dir.stem
+
+        orig_audio, sampling_rate = load_audio(orig_dir)
+
+        subplot(2, 2, 1)
+        waveshow(orig_audio, sr=sampling_rate)
+        title("Original")
+
+        subplot(2, 2, 3)
+        orig_spec = stft(orig_audio)
+        orig_spec = amplitude_to_db(abs(orig_spec))
+        specshow(orig_spec, sr=sampling_rate)
+
+        clean_name = f"{audio_name}_clean"
+        clean_dir = orig_dir.parent.joinpath("clean", clean_name + ".wav")
+        clean_audio, sampling_rate = load_audio(clean_dir)
+
+        subplot(2, 2, 2)
+        waveshow(clean_audio, sr=sampling_rate)
+        title("Clean")
+
+        subplot(2, 2, 4)
+        clean_spec = stft(clean_audio)
+        clean_spec = amplitude_to_db(abs(clean_spec))
+        specshow(clean_spec, sr=sampling_rate)
+
+        suptitle(f'Waveform & spectrogram of "{audio_name}"')
+        show()
 
 
 chs = CleanHeartSounds()
@@ -77,34 +164,5 @@ clean = chs.clean_heart_sounds(
     model_input_shape=800,
 )
 
-
-def show_wave_n_spec(audiofile_dir: str) -> None:
-    """
-    Import audio file and show its waveform and
-    spectogram representation.
-
-    Args:
-        audiofile_dir: directory string name of
-            the single heartsound audio file
-
-    Returns: None, waveform and spectogram is displayed
-    """
-
-    audiofile_dir = Path(audiofile_dir)
-    spf = open_wave(str(audiofile_dir), "r")
-
-    sound_info = spf.readframes(-1)
-    sound_info = frombuffer(sound_info, int16)
-
-    subplot(211)
-    plot(sound_info)
-    title(f'Waveform & spectrogram of "{audiofile_dir.stem}"')
-
-    f = spf.getframerate()
-    subplot(212)
-    specgram(sound_info, Fs=f, scale_by_freq=True, sides="default")
-
-    show()
-    spf.close()
-
-# show_wave_n_spec("Data/predict/b0061.wav")
+chs.compare_clean("Data/predict/PHS.wav")
+chs.compare_clean("Data/predict/ICBHI.wav")
